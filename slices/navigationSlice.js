3e780af9ad379409540f836025dd52d2a286e079
uzair7886 <jerry.khan7886@gamil.com>
2023-07-14 22:36:01 +0500

    initial commit

---
+++ slices/navigationSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  origin:null,
+    destination:null,
+    travelTimeInformation:null,
+}
+
+export const navigationSlice = createSlice({
+  name: 'navigation',
+  initialState,
+  reducer: {
+    setOrigin:(state,action)=>{
+        state.origin=action.payload;
+    },
+    setDestination:(state,action)=>{
+        state.destination=action.payload;
+    },
+    setTravelTimeInformation:(state,action)=>{
+        state.travelTimeInformation=action.payload;
+    }
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { setOrigin,setDestination,setTravelTimeInformation } = navigationSlice.actions
+
+export const selectOrigin=(state)=>state.navigation.origin
+
+export const selectDestination=(state)=>state.navigation.destination
+
+export const selectTravelTimeInformation=(state)=>state.navigation.travelTimeInformation
+
+export default navigationSlice.reducer
---
+++ store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import  navigationSlice  from './slices/navigationSlice'
+
+export const store = configureStore({
+    reducer: {
+        nav: navigationSlice,
+    },
+  })
